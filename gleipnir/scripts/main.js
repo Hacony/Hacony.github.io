f8fd4fd1098bf57b9e63378b1b4b7a9da053894e
Gabe Perry <twistedsnakes@live.com>
2021-04-29 00:18:23 +0800

    Add Array.map polyfill to Gleipnir page

--- gleipnir/scripts/main.js
+++ gleipnir/scripts/main.js
@@ -1,3 +1,6 @@
+// Array map polyfill
+Array.prototype.map||(Array.prototype.map=function(r){var t,n,o;if(null==this)throw new TypeError("this is null or not defined");var e=Object(this),i=e.length>>>0;if("function"!=typeof r)throw new TypeError(r+" is not a function");for(arguments.length>1&&(t=arguments[1]),n=new Array(i),o=0;o<i;){var a,p;o in e&&(a=e[o],p=r.call(t,a,o,e),n[o]=p),o++}return n});
+
 (async () => {
 const dataJsonUrl = 'data.json';
 const skipAnimations = false;
